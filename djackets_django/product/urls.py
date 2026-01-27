99725dfbbf1eb0256d1c59dedec010e169e6f953
aaronmubiru <aaron.mubiru@qubersavings.com>
2026-01-27 14:05:39 -0400

    Configured New Django class based view for product details and mapped its appropriate url to it in urls.py file for the products app

--- djackets_django/product/urls.py
+++ djackets_django/product/urls.py
@@ -5,6 +5,8 @@ from product import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    #MAPPING URL PATH FOR PRODUCT DETAILS TO USE NEWLY MADE class based view ProductDetails as view 
+    path('products/<slug:category_slug>/<slug:product_slug>/',views.ProductDetail.as_view())
 ]
 
 
--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,7 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response, Serializer
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -24,5 +25,25 @@ class LatestProductsList(APIView):
         serializer = ProductSerializer(products, many=True)
 
         #Now after serializing/ converting the Object model to JSON we return it as the response
+        return Response(serializer.data)
+
+
+#Creating a new viewset class to handle returning the details of objects
+class ProductDetail(APIView):
+
+    #HAVING INHERITED FROM THE API VIEW WE ARE NOT GOING TO OVERIDE THE GET FUNCTION BUT INSTEAD OVERRIDE THE GET OBJECT FUNCTION AND PASS IN 
+    def get_object(self, category_slug,product_slug):
+        try:
+            return Product.objects.filter(category_slug = category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    #NOW OVERRIDE THE GET FUNCTION TO USE THE ABOVE OVERRIDEN get_object function
+    def get(self, request, category_slug, product_slug,format=None):
+        product = self.get_object(category_slug, product_slug)
+        #Still want to use the serializer to convert the object to JSON ofcourse
+        serializer = ProductSerializer(product)
+
+        #Return the JSON
         return Response(serializer.data)
 
